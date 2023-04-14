e5a857b28e41b23d768dc6bbb3adce20ea161707
mikaerdmann <mikaerdmann.2@gmail.com>
2023-04-14 16:22:23 +0200

    7.7 inverse ea 2 updates

--- EA_inversePyoo_2.py
+++ EA_inversePyoo_2.py
@@ -22,7 +22,7 @@ class Ramsey(ElementwiseProblem):
 
     def _evaluate(self, x, out, *args, **kwargs):
         out["F"] = ramsey_inverse(x)
-        out["G"] =
+
 
 
 def ramsey_inverse(x):
@@ -30,9 +30,9 @@ def ramsey_inverse(x):
     # results = inverse_functions.run_model1_inverse(timeswitch=2, vm_weight=x[0], vm_cumdepr_new_inverse=x[1], vm_cumdepr_old_inverse=x[2])
     pm_welf = [5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 7.5, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0]
     # pm_welf[9 - int(n / 2):8 + int(n / 2)] = x
-    pm_welf[8 - a:8 + b] = X[0]
-    cumdepr_new = pm_cumdepr_new_array[8 - a:8 + b] = X[1]
-    cumdepr_old = pm_cumdepr_old_array[8 - a:8 + b] = X[2]
+    pm_welf[8 - a:8 + b] = X[0][8 - a:8 + b]
+    cumdepr_new = X[1]
+    cumdepr_old  = X[2]
     results = inverse_functions.run_model1_inverse(timeswitch=2, vm_weight=pm_welf,c_n = cumdepr_new, c_o = cumdepr_old)
     vm_run = [inverse_functions.get_val(results.vm_cons), inverse_functions.get_val(results.vm_cesIO),
               inverse_functions.get_val(results.vm_invMacro)]
@@ -55,13 +55,13 @@ Ti = [0,1]
 As = [8]
 Bs = [10]
 ABs = np.asarray([As,Bs])
-Residuals_all = np.empty((2,4))
+Residuals_all = np.empty((1,ABs.shape[1]))
 Residuals_t = np.empty((1,18))
 for i in range(ABs.shape[1]):
     a = ABs[0][i]
     b = ABs[1][i]
     N = np.arange(8 - a, 8 + b).size
-    n = N*3
+    n = 18*2+N
     for t in Ti:
         if __name__ == "__main__" and t == 1:
             vm_opt = EA_inverse.get_vm_opt(1)
@@ -108,25 +108,26 @@ for i in range(ABs.shape[1]):
             vm_opt = EA_inverse.get_vm_opt(1)
             np.random.seed(1)
             Problem = Ramsey()
-            x0 = np.asarray([pm_welf[8 - a:8 + b], pm_cumdepr_new_array[8 - a:8 + b] , pm_cumdepr_old_array[8 - a:8 + b]]).flatten()
-            algorithm = CMAES(x0=x0, sigma=0.1, restarts=11, restart_from_best=True, bipop=True)
+            x0 = np.asarray([pm_welf[8 - a:8 + b], pm_cumdepr_new_array , pm_cumdepr_old_array]).flatten()
+            algorithm = CMAES(x0=x0, sigma=0.09, restarts=11, restart_from_best=True, bipop=True)
             # termination = DefaultSingleObjectiveTermination(xtol=1e-8,cvtol=1e-6,ftol=1e-6,period=20,n_max_gen=1000,n_max_evals=100000)
-            termination = get_termination("time", "00:08:00")
+            termination = get_termination("time", "00:25:00")
 
             # algorithm = NSGA2()
             # res = minimize(Problem,algorithm,seed=1, x0=np.random.random(Problem.n_var), verbose = True)
             res = minimize(Problem, algorithm, termination, verbose=True)
-            x = list(res.X)
-            pm_welf[8 - a:8 + b] = x
+            x =np.asarray(list(res.X))
+            x = np.reshape(x, (3, N))
+            pm_welf[8 - a:8 + b] = x[0]
             results = inverse_functions.run_model1_inverse(timeswitch=2, vm_weight=pm_welf, c_o=0, c_n=0)
             vm_run = [inverse_functions.get_val(results.vm_cons), inverse_functions.get_val(results.vm_cesIO),
                       inverse_functions.get_val(results.vm_invMacro)]
             tall_string = model1_functions.f_tall_string_b()
             tall_int = [int(i) for i in tall_string]
-            residuals1 = np.sqrt(
-                sum((vm_opt[0][8 - a:8 + b] - vm_run[0][8 - a:8 + b]) ** 2, 1) + sum(
-                    (vm_opt[1][8 - a:8 + b] - vm_run[1][8 - a:8 + b]) ** 2, 1) + sum(
-                    (vm_opt[2][8 - a:8 + b] - vm_run[2][8 - a:8 + b]) ** 2, 1))
+            #residuals1 = np.sqrt(
+            #    sum((vm_opt[0][8 - a:8 + b] - vm_run[0][8 - a:8 + b]) ** 2, 1) + sum(
+            #        (vm_opt[1][8 - a:8 + b] - vm_run[1][8 - a:8 + b]) ** 2, 1) + sum(
+            #        (vm_opt[2][8 - a:8 + b] - vm_run[2][8 - a:8 + b]) ** 2, 1))
             # residuals1 = np.sqrt(
             #     sum((vm_opt[0][9 - int(n / 2):8 + int(n / 2)] - vm_run[0][9 - int(n / 2):8 + int(n / 2)]) ** 2, 1) + sum(
             #         (vm_opt[1][9 - int(n / 2):8 + int(n / 2)] - vm_run[1][9 - int(n / 2):8 + int(n / 2)]) ** 2, 1) + sum(
@@ -137,12 +138,17 @@ for i in range(ABs.shape[1]):
             residuals3 = np.sqrt((vm_opt[0] - vm_run[0]) ** 2 + (vm_opt[1] - vm_run[1]) ** 2 + (vm_opt[2] - vm_run[2]) ** 2)
 
             print(f"The residuals over the whole time are {residuals2}")
-            print(f"The residuals per time step are {residuals3}")
-            Residuals_all[0][i] = residuals1
-            Residuals_all[1][i] = residuals2
+            #print(f"The residuals per time step are {residuals3}")
+            Residuals_all[i] = residuals2
             Residuals_t[i] = residuals3
             # Axis creation
             fig3, axs = plt.subplots(3, 2)
+            axs[0,0].set_ylim([0, 10])
+            axs[0,1].set_ylim([0, 10])
+            axs[1,0].set_ylim([0, 45])
+            axs[1,1].set_ylim([0, 45])
+            axs[2,0].set_ylim([-10, 10])
+            axs[2,1].set_ylim([-10, 10])
             axs[0, 0].plot(tall_int, vm_opt[0], 'b')
             axs[0, 0].legend("Consumption optimal")
             axs[0, 1].plot(tall_int, vm_run[0], 'b')
@@ -155,13 +161,13 @@ for i in range(ABs.shape[1]):
             axs[2, 0].legend(("Investment Optimal"), loc='upper right')
             axs[2, 1].plot(tall_int, vm_run[2], "r")
             axs[2, 1].legend(("Investment run"), loc='upper right')
-            fig3.suptitle(f"Pyomo model using optimized pm_welf with n = {ni} and optimizing over all residuals.")
+            fig3.suptitle(f"Pyomo model using optimized pm_welf with n = {N} and optimizing over all residuals.")
 
             fig2, axs = plt.subplots(1,2)
             axs[0].plot(tall_int, residuals3)
             fig2.suptitle("Residuals over K, C, I per time step")
-        fig.savefig(f"C:\\Users\\mikae\\Documents\\Uni\\Project 1\\Results\\06042023\\Benchmark_n{n}.png")
-        fig2.savefig(f"C:\\Users\\mikae\\Documents\\Uni\\Project 1\\Results\\06042023\\Opt_n{n}.png")
-        fig3.savefig(f"C:\\Users\\mikae\\Documents\\Uni\\Project 1\\Results\\06042023\\Residuals_opt_n{n}.png")
-        print(Residuals_all)
-        print(Residuals_t)
+        #   fig.savefig(f"C:\\Users\\mikae\\Documents\\Uni\\Project 1\\Results\\06042023\\Benchmark_n{n}.png")
+        #   fig2.savefig(f"C:\\Users\\mikae\\Documents\\Uni\\Project 1\\Results\\06042023\\Opt_n{n}.png")
+        #   fig3.savefig(f"C:\\Users\\mikae\\Documents\\Uni\\Project 1\\Results\\06042023\\Residuals_opt_n{n}.png")
+        print(f"Residuals all: {Residuals_all}")
+        print(f"Residuals per timestep: {Residuals_t}")
